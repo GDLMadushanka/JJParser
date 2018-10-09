cfe4b6a2e96ff3c2f6bab037ecc0307e6c7b7237
GDLMadushanka <gdlmadushanka@gmail.com>
2018-10-09 15:52:16 +0530

    Finalyze array validation and test cases

--- src/test/java/testValidators/TestArrayValidator.java
+++ src/test/java/testValidators/TestArrayValidator.java
@@ -50,8 +50,8 @@ public class TestArrayValidator {
     public void testArrayOfItemsValid() throws ValidatorException, ParserException {
         String schema = "{ \"type\": \"array\", \"items\":[{\"type\":\"integer\"},{ \"type\": \"string\"," +
                 "\"minLength\": 6},{ \"type\": \"boolean\" ,\"const\":true}]}";
-        String testPayload = "[\"8\",\"Lahiru\",\"true\"]";
-        String expectedPayload = "[8,\"Lahiru\", true]";
+        String testPayload = "[\"8\",\"Lahiru\",\"true\",\"Additional\"]";
+        String expectedPayload = "[8,\"Lahiru\", true, \"Additional\"]";
         JsonObject schemaObject = (JsonObject) parser.parse(schema);
         JsonArray expected = (JsonArray) parser.parse(expectedPayload);
         JsonArray result = ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
@@ -118,6 +118,154 @@ public class TestArrayValidator {
         Assert.assertNotNull("Validator didn't respond with a JSON primitive", result);
         Assert.assertEquals("Didn't receive the expected primitive", expected, result);
     }
-}
 
-//todo debug logs and more comments
+    /**
+     * This test checks an array for valid minItems constraint.
+     */
+    @Test
+    public void testValidMinItemsConstraint() throws ValidatorException, ParserException {
+        String schema = "{ \"type\": \"array\", \"items\":{ \"type\": \"string\"},\"minItems\":2}";
+        String testPayload = "[\"You\",\"and\",\"I\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        JsonArray expected = (JsonArray) parser.parse(testPayload);
+        JsonArray result = ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+        Assert.assertNotNull("Validator didn't respond with a JSON primitive", result);
+        Assert.assertEquals("Didn't receive the expected primitive", expected, result);
+    }
+
+    /**
+     * This test checks an array for invalid minItems constraint.
+     */
+    @Test
+    public void testInvalidMinItemsConstraint() throws ValidatorException, ParserException {
+        thrown.expect(ValidatorException.class);
+        String schema = "{ \"type\": \"array\", \"items\":{ \"type\": \"string\"},\"minItems\":3}";
+        String testPayload = "[\"Too\",\"Late\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+    }
+
+    /**
+     * This test checks an array for valid maxItems constraint.
+     */
+    @Test
+    public void testValidMaxItemsConstraint() throws ValidatorException, ParserException {
+        String schema = "{ \"type\": \"array\", \"items\":{ \"type\": \"string\",\"enum\":[\"You\",\"I\",\"and\"]}," +
+                "\"maxItems\":3}";
+        String testPayload = "[\"You\",\"and\",\"I\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        JsonArray expected = (JsonArray) parser.parse(testPayload);
+        JsonArray result = ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+        Assert.assertNotNull("Validator didn't respond with a JSON primitive", result);
+        Assert.assertEquals("Didn't receive the expected primitive", expected, result);
+    }
+
+    /**
+     * This test checks an array for invalid maxItems constraint.
+     */
+    @Test
+    public void testInvalidMaxItemsConstraint() throws ValidatorException, ParserException {
+        thrown.expect(ValidatorException.class);
+        String schema = "{ \"type\": \"array\", \"items\":{ \"type\": \"string\"},\"maxItems\":3}";
+        String testPayload = "[\"What\",\"I\",\"Have\",\"Done\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+    }
+
+    /**
+     * This test checks an array for valid uniqueItems constraint.
+     */
+    @Test
+    public void testValidUniqueItemsConstraint() throws ValidatorException, ParserException {
+        String schema = "{ \"type\": \"array\", \"items\":{ \"type\": \"boolean\"},\"uniqueItems\":true}";
+        String testPayload = "[\"true\",\"false\"]";
+        String expectedPayload = "[true,false]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        JsonArray expected = (JsonArray) parser.parse(expectedPayload);
+        JsonArray result = ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+        Assert.assertNotNull("Validator didn't respond with a JSON primitive", result);
+        Assert.assertEquals("Didn't receive the expected primitive", expected, result);
+    }
+
+    /**
+     * This test checks an array for invalid uniqueItems constraint.
+     */
+    @Test
+    public void testInvalidUniqueItemsConstraint() throws ValidatorException, ParserException {
+        thrown.expect(ValidatorException.class);
+        String schema = "{ \"type\": \"array\", \"items\":{ \"type\": \"boolean\"},\"uniqueItems\":true}";
+        String testPayload = "[\"true\",\"false\",\"true\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+    }
+
+    /**
+     * This test checks additionalItems true constraint
+     */
+    @Test
+    public void testAdditionalItemsTrue() throws ValidatorException, ParserException {
+        String schema = "{ \"type\": \"array\", \"items\":[{\"type\":\"integer\"},{ \"type\": \"string\"},{\"type\": " +
+                "\"boolean\"}],\"additionalItems\":true}";
+        String testPayload = "[\"8\",\"Lahiru\",\"true\",\"true\"]";
+        String expectedPayload = "[8,\"Lahiru\", true, \"true\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        JsonArray expected = (JsonArray) parser.parse(expectedPayload);
+        JsonArray result = ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+        Assert.assertNotNull("Validator didn't respond with a JSON primitive", result);
+        Assert.assertEquals("Didn't receive the expected primitive", expected, result);
+    }
+
+    /**
+     * This test checks additionalItems false constraint
+     */
+    @Test
+    public void testAdditionalItemsFalse() throws ValidatorException, ParserException {
+        thrown.expect(ValidatorException.class);
+        String schema = "{ \"type\": \"array\", \"items\":[{\"type\":\"integer\"},{ \"type\": \"string\"},{\"type\": " +
+                "\"boolean\"}],\"additionalItems\":false}";
+        String testPayload = "[\"8\",\"Lahiru\",\"true\",\"true\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload), schemaObject);
+    }
+
+    /**
+     * This test checks additionalItems as an empty object
+     */
+    @Test
+    public void testAdditionalItemsAsEmptyObject() throws ValidatorException, ParserException {
+        String schema = "{ \"type\": \"array\", \"items\":[{\"type\":\"integer\"},{ \"type\": \"string\"},{\"type\": " +
+                "\"boolean\"}],\"additionalItems\":{}}";
+        String testPayload = "[\"8\",\"Lahiru\",\"true\",\"true\"]";
+        String expectedPayload = "[8,\"Lahiru\", true, \"true\"]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        JsonArray expected = (JsonArray) parser.parse(expectedPayload);
+        JsonArray result = ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+        Assert.assertNotNull("Validator didn't respond with a JSON primitive", result);
+        Assert.assertEquals("Didn't receive the expected primitive", expected, result);
+    }
+
+    /**
+     * This test checks additionalItems as an empty an array
+     */
+    @Test
+    public void testAdditionalItemsAsArray() throws ValidatorException, ParserException {
+        String schema = "{ \"type\": \"array\", \"items\":[{\"type\":\"integer\"},{ \"type\": \"string\"},{\"type\": " +
+                "\"boolean\"}],\"additionalItems\":{\"type\":\"integer\"}}";
+        String testPayload = "[\"8\",\"Lahiru\",\"true\",\"12\",\"13\"]";
+        String expectedPayload = "[8,\"Lahiru\", true, 12, 13]";
+        JsonObject schemaObject = (JsonObject) parser.parse(schema);
+        JsonArray expected = (JsonArray) parser.parse(expectedPayload);
+        JsonArray result = ArrayValidator.validateArray(GSONDataTypeConverter.getMapFromString(testPayload),
+                schemaObject);
+        Assert.assertNotNull("Validator didn't respond with a JSON primitive", result);
+        Assert.assertEquals("Didn't receive the expected primitive", expected, result);
+    }
+}
